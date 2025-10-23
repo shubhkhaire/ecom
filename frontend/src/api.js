e0fca2ec73d5769cc9bc9f14aaefa89fb98ef929
Shubham Khaire <shubhamkhaire523@gmail.com>
2025-10-24 00:22:04 +0530

    Configured frontend for Render deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_URL = process.env.REACT_APP_API_URL;
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: API_URL,
 });
 
 export default API;
