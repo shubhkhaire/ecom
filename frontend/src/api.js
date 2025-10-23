6384bd498288882921dd3b079b9519134a4abb71
Shubham Khaire <shubhamkhaire523@gmail.com>
2025-10-24 00:35:32 +0530

    Configured frontend for Render deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
-const API_URL = process.env.REACT_APP_API_URL;
+
+const API_URL =
+  process.env.REACT_APP_API_URL || "https://ecom-backend-c92s.onrender.com";
 
 const API = axios.create({
   baseURL: API_URL,
+  withCredentials: true, // important for cookies/session
 });
 
 export default API;
