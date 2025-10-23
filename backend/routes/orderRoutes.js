b275888962dfae9373c2d5746569df9bcec90346
Shubham Khaire <shubhamkhaire523@gmail.com>
2025-10-23 23:38:11 +0530

    Fix: lowercase orderController filename for Render Linux

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createOrder, getOrders } from "../controllers/orderController.js";
+import { createOrder } from "../controllers/orderController.js";
 const router = express.Router();
 
 
